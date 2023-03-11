bec4a57c81df6c4b61d86184f1219b057ac8cfc3
xT0mR0x <112822060+xT0mR0x@users.noreply.github.com>
2023-03-11 04:35:07 +0200

    ...

---
+++ layouts/partials/footer/particles.js
@@ -0,0 +1,11 @@
+(function() {
+  var script = document.createElement('script');
+  script.onload = function() {
+    // particlesJS loaded
+  };
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js';
+  script.integrity = 'sha512-Kef5sc7gfTacR7TZKelcrRs15ipf7+t+n7Zh6mKNJbmW+/RRdCW9nwfLn4YX0s2nO6Kv5Y2ChqgIakaC6PW09A==';
+  script.crossOrigin = 'anonymous';
+  script.referrerPolicy = 'no-referrer';
+  document.head.appendChild(script);
+})();
