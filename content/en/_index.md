cf7bf2f1501608c76563b0af549652c234fd18bb
xT0mR0x <112822060+xT0mR0x@users.noreply.github.com>
2023-03-13 02:04:20 +0200

    ...

--- content/en/_index.md
+++ content/en/_index.md
@@ -9,9 +9,3 @@ images: []
 <img src="IMG_1660 2.jpeg"  width="120" height="120" alt="Square" class="border-0 rounded-circle mr-3" style="margin-bottom: 50px;">
 
  -----
-
-<br><br>
-	<link rel="stylesheet" href="style.css">
-    <div id="text-container"></div>
-    <script src="animation.js"></script>
-<br><br>
--- static/animation.js
+++ static/animation.js
@@ -1,7 +1,6 @@
 // animation.js
 // JavaScript code to set the text
-var text = "Wellcom!\n\nMy name is Tom Rosenzweig, I am an ambitious and aspiring security researcher and analyst. My journey into the field of Cyber-Security and IT began in 2021, and since then, I have been dedicated to developing my skills and expertise in this dynamic and ever-evolving industry. \n\n The purpose of creating this website is to share my documentation and projects, and to create a centralized toolkit by compiling and organizing all the necessary resources in one place, this website serves as a valuable reference point for me to quickly access the tools, techniques, and knowledge I need for my work in the industry. It is my hope that it can also serve as a useful resource for others in the field.\n\nThank you for visiting! If you have any questions or comments, please don't hesitate to contact me via Linkedin and GitHub on the links below!";
-
+var text = "My name is Tom Rosenzweig, I am an ambitious and aspiring security researcher and analyst. My journey into the field of Cyber-Security and IT began in 2021, and since then, I have been dedicated to developing my skills and expertise in this dynamic and ever-evolving industry. \n\n The purpose of creating this website is to share my documentation and projects, and to create a centralized toolkit by compiling and organizing all the necessary resources in one place, this website serves as a valuable reference point for me to quickly access the tools, techniques, and knowledge I need for my work in the industry. It is my hope that it can also serve as a useful resource for others in the field.";
 // Set the text content of the container element
 var container = document.getElementById("text-container");
 container.innerHTML = "";
